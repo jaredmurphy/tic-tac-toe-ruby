2bcd195d3076357b602a3897956e53c91d16a29f
Jared Murphy <jaredcmurphy@gmail.com>
2017-04-29 23:21:21 -0400

    refactored a lot

--- lib/build.rb
+++ lib/build.rb
@@ -1,6 +1,7 @@
 require "pry"
 
 module Build
+    # use of polymorphism
     class Board < Array
         def initialize(size)
             @size = size
@@ -12,31 +13,36 @@ module Build
         end
 
         private
-        
+
         def make_tiles
-            @size.times { self.push([])} # makes @size number of columns
-            self.map! do |row|  # makes @size number of rows in each column
+            @size.times {self.push([])} # makes @size number of rows
+            self.map! do |row|  # makes @size number of columns in each row
                 @size.times {row.push(Build::Tile.new)}
                 row
             end
         end
 
         def printable_board
-            board = self.map do |tile|
-                tile.map {|row| row}.join(" | ")
-            end
-            board.join("\n")
+            # board_lines makes the lines between each position on the board
+            board_lines = self.map do |tile|
+                tile.map {|row| row }.join(" | ")
+            end.join("\n")
+            #guide makes column numbers at the top of the board for reference
+            guide = (0..@size - 1).collect {|i| "#{i + 1}   "}.join("")
+            "#{guide}\n#{board_lines}"
         end
     end
 
+
+    # use of polymorphism
     class Tile < String
         attr_accessor :value
         def initialize
             @value = 0
-            self.concat(" ")
+            self.replace(" ")
         end
 
-        def blank?
+        def blank? # called in Helper:available_tiles
             self.match(/\s/) ? true : false
         end
 
